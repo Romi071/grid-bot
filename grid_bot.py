a8524b63667b2996c42b45cc3bc60ec54bf1f015
Romi <romi.mateos2005@gmail.com>
2026-08-25 16:06:11 +0200

    finished the order dispatching for a n-level grid

--- grid_bot.py
+++ grid_bot.py
@@ -10,6 +10,10 @@ api_secret = os.getenv("API_SECRET")
 #Authenticate to the Testnet
 client = Client(api_key, api_secret, testnet=True)
 
+#Cancel all open orders to make the script safely runnable
+client.cancel_all_open_orders(symbol="BTCUSDT")
+print(client.get_open_orders(symbol="BTCUSDT"))
+
 #Pull live sandbox balance & BTC ticker
 usdt_balance = client.get_asset_balance(asset = "USDT")
 ticker = client.get_symbol_ticker(symbol="BTCUSDT")
@@ -20,10 +24,36 @@ free_usdt = float(usdt_balance["free"])
 print(f"💰 Available USDT: ${free_usdt:.2f}")
 print(f"📈 Current BTC Price: ${current_price:.2f}")
 
-#Get our BTC fraction share and 1% steps for the grid bot
+#Define the n- buy and sell levels with grid steps of 1%
 grid_step = 0.01
-sell_level = current_price + current_price * grid_step
-buy_level = current_price - current_price * grid_step
+n = 3
+buy_levels = []
+sell_levels = []
+for k in range(1, n+1):
+    buy_levels.append(current_price * (1 - k * grid_step))
+    sell_levels.append(current_price * (1 + k * grid_step))
 
-print(f"🟢 Buy Target:  ${buy_level:.2f}")
-print(f"🔴 Sell Target: ${sell_level:.2f}")
+#Order dispatching:
+usdt_per_order = 15
+
+#Buy loop
+for price in buy_levels:
+    buy_price = str(round(price, 2))
+    btc_per_order = str(round(usdt_per_order / price, 5))
+
+    order = client.order_limit_buy(
+    symbol="BTCUSDT",
+    quantity=btc_per_order,
+    price=buy_price
+    )
+
+#Sell Loop
+for price in sell_levels:
+    sell_price = str(round(price, 2))
+    btc_per_order = str(round(usdt_per_order / price, 5))
+
+    order = client.order_limit_sell(
+    symbol="BTCUSDT",
+    quantity=btc_per_order,
+    price=sell_price
+    )
